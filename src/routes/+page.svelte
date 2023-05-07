b62a206f1aa3dcdf3939ba5201f4344a705a56ad
SkepticMystic <rossk29@gmail.com>
2023-05-07 11:37:05 +0200

    Hero page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,19 @@
 <script lang="ts">
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<div class="hero mt-32">
+  <div class="hero-content text-center">
+    <div class="max-w-lg">
+      <h1 class="text-5xl font-bold">Generic App</h1>
+
+      <div class="flex flex-wrap justify-center gap-5 my-14">
+        <a href="/projects">
+          <button class="btn btn-primary">Projects</button>
+        </a>
+        <a href="/tasks">
+          <button class="btn btn-secondary">Tasks</button>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
