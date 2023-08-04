2d7f69ed89b3710e0b82ae787e3f26bb97ad8355
SkepticMystic <rossk29@gmail.com>
2023-08-04 09:52:04 +0200

    Only invalidateSession if there is one

--- src/routes/api/signout/+server.ts
+++ src/routes/api/signout/+server.ts
@@ -1,13 +1,15 @@
 import { auth } from "$lib/auth/lucia";
-import { error, redirect } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
 export const POST: RequestHandler = async ({ locals }) => {
   const session = await locals.auth.validate();
-  if (!session) throw error(401, "Not logged in");
 
-  await auth.invalidateSession(session.sessionId); // invalidate session
-  locals.auth.setSession(null); // remove cookie
+  if (session) {
+    await auth.invalidateSession(session.sessionId); // invalidate session
+  }
+
+  locals.auth.setSession(null);
 
   throw redirect(302, "/signin");
 };
